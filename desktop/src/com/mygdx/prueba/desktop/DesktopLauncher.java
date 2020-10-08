2fd5444dec34213c7a0343f27a5214af2165cd50
aap <arturoagudo@hotmail.com>
2020-10-08 15:50:36 +0200

    PRUEBA DE ELIMINACION

--- desktop/src/com/mygdx/prueba/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/prueba/desktop/DesktopLauncher.java
@@ -4,10 +4,3 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.prueba.PruebaInstaladorPropio;
 
-public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new PruebaInstaladorPropio(), config);
-		System.out.println("Hola Feo!!");
-	}
-}
